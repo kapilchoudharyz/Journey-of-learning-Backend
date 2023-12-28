a33ebee0264b0c32e06b9cdd33c0f3c4e81df3d2
kapil choudhary <kapilchoudhary0123.com>
2023-12-28 20:54:36 +0530

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 // require('dotenv').config({'paht: ./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
-
-
-
-
+connectDB()//Returns a promise
+  .then(() => {
+    app.on('error', (err)=>{
+      console.log('ERROR', err);
+      throw err
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+    
+  })
+  .catch((err) => console.log(`Mongodb connection failed !!! : ${err}`));
 
 
 
